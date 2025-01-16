138f8e71421bf10436ddbac5162abad81bed2ebd
StevieG223 <77689111+StevieG223@users.noreply.github.com>
2025-01-16 14:44:12 -0500

    incoporate project class into project display

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "./styles.css";
-import {Project} from "./project-module.js"
+import {Project, myProjects} from "./project-module.js"
+
+function populateProjects(arr){
+    const projectsSection = document.querySelector(".projects");
+    projectsSection.replaceChildren();
+    arr.forEach(project => {
+        let projectEntry = document.createElement('div');
+        projectEntry.classList.add("project-card");
+        projectEntry.innerHTML = `<h3 class="project-name">${project.title}</h3>
+                    <p>Due Date: ${project.dueDate}</p>
+                    <p>${project.priority}</p>
+                    <p>Desciption: ${project.description}</p>
+                    <div class="buttons">
+                        <button>Complete</button>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>`;
+        projectsSection.appendChild(projectEntry);
+    });
+}
+
+let projectArray = myProjects.getProjects();
+console.log(projectArray);
+populateProjects(projectArray);
--- src/project-module.js
+++ src/project-module.js
@@ -1,4 +1,4 @@
-export {Project}
+export {Project, myProjects}
 class Project{
     constructor(title, description, dueDate, priority, notes){
         this.title = title;
@@ -8,3 +8,18 @@ class Project{
         this.notes = notes;
     }
 }
+
+function intializeProjects(arr){
+    function addProject(project){
+        arr.push(project)
+    }
+    const getProjects = () => arr;
+    return {getProjects, addProject};
+}; 
+
+const myProjects = intializeProjects([]);
+let firstProject = new Project("Update PowerPoints", "update your PowerPoints for spring classes", "Jan 13, 2025", "medium", "remember to alter dates and update project descriptions on slides.");
+myProjects.addProject(firstProject);
+let secondProject = new Project("Update syllabus", "update your 110 and 111 syllabi", "Jan 10, 2025", "urgent", "update project descriptions and calendar.");
+myProjects.addProject(secondProject);
+
