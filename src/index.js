8833a480ba047cc335ed8b3493d68b093e884541
StevieG223 <77689111+StevieG223@users.noreply.github.com>
2025-01-20 15:35:06 -0500

    Add card and some button behavior

--- src/index.js
+++ src/index.js
@@ -8,18 +8,35 @@ function populateProjects(arr){
         let projectEntry = document.createElement('div');
         projectEntry.classList.add("project-card");
         projectEntry.innerHTML = `<h3 class="project-name">${project.title}</h3>
-                    <p>Due Date: ${project.dueDate}</p>
                     <p>${project.priority}</p>
-                    <p>Desciption: ${project.description}</p>
                     <div class="buttons">
-                        <button>Complete</button>
-                        <button>Edit</button>
-                        <button>Delete</button>
+                        <button class='details'>Details</button>
                     </div>`;
         projectsSection.appendChild(projectEntry);
     });
+    const detailsButtons = Array.from(document.getElementsByClassName("details"));
+    detailsButtons.forEach(button =>{
+        button.addEventListener("click", (eventObj)=> {
+            let expandedProject = eventObj.target.parentElement.parentElement;
+            let projectName = expandedProject.firstChild.innerHTML;
+            console.log(projectName)
+            expandedProject.classList.add("expanded-card");
+            expandedProject.replaceChildren();
+            let projectIndexPos = arr.findIndex(proj=> proj.title == projectName);
+            let project = arr[projectIndexPos];
+            expandedProject.innerHTML = ` <h3 class="project-name">${project.title}</h3>
+            <p>Due Date: ${project.dueDate}</p>
+            <p>${project.priority}</p>
+            <p>Description: ${project.description}</p>
+            <p>Additional Notes: ${project.notes}</p>
+            <div class="buttons">
+                <button class='complete'>Complete</button>
+                <button class='edit'>Edit</button>
+                <button class='delete'>Delete</button>
+            </div>`;
+        });           
+});
 }
 
 let projectArray = myProjects.getProjects();
-console.log(projectArray);
 populateProjects(projectArray);
